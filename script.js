44f6acd8e1e8c1f5187969cfffc05621c0ccfb0b
naijoesrinivasan <naijoesrinivasan99@gmail.com>
2024-07-20 20:05:21 -0700

    updated script.js for  button click alert

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-function setWidth() {
-    const button = document.querySelector('button');
-    const pTag = document.getElementById('button-parent');
-
-    console.log(button.style.width);
-    console.log(pTag.style.width);
-    button.style.width = pTag.style.width;
+function alertNoResponse() {
+    window.alert('Button is for illustrative purposes');
 }
